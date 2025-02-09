585dcf308e8a580458450ea2ef22f78853400eb1
Viktor Zhdanovych <cryingtm27@gmail.com>
2025-02-09 08:36:35 +0200

    add __str__ to models

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class TaskType(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     class Priority(models.TextChoices):
@@ -14,7 +17,7 @@ class Task(models.Model):
         LOW = "LOW", "Low"
 
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     deadline = models.DateField()
     is_complete = models.BooleanField(default=False)
     priority = models.CharField(
@@ -26,36 +29,71 @@ class Task(models.Model):
         "TaskType",
         on_delete=models.CASCADE,
     )
-    assignees = models.ManyToManyField("Worker", related_name="assignees")
+    assignees = models.ManyToManyField(
+        "Worker",
+        related_name="assignees",
+        blank=True
+    )
+
+    class Meta:
+        ordering = ["date"]
+
+    def __str__(self):
+        return f"{self.name} {self.deadline} {self.priority} {self.is_complete}"
 
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
-    tasks = models.ForeignKey(
-        "Task",
-        on_delete=models.CASCADE,
+    description = models.TextField(
+        max_length=500,
+        blank=True,
     )
-    description = models.TextField()
+    tasks = models.ManyToManyField(
+        "Task",
+        related_name="projects",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Worker(AbstractUser):
     position = models.ForeignKey(
         "Position",
         on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
+
+    class Meta:
+        verbose_name = "Worker"
+        verbose_name_plural = "Workers"
+
+    def __str__(self):
+        return f"{self.username}({self.first_name} {self.last_name}) {self.position}"
 
 
 class Position(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    members = models.ForeignKey(
-        "Worker",
-        on_delete=models.CASCADE,
-    ),
-    project = models.ForeignKey(
+    members = models.ManyToManyField(
+        Worker,
+        related_name="members",
+        blank=True,
+    )
+    projects = models.ForeignKey(
         "Project",
         on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
+
+    def __str__(self):
+        return self.name
