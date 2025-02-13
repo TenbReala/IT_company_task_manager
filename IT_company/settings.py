a660eff0926988b3aa714fa381274fc78f29b163
Viktor Zhdanovych <cryingtm27@gmail.com>
2025-02-13 15:06:31 +0200

    update settings for debug toolbar

--- IT_company/settings.py
+++ IT_company/settings.py
@@ -138,3 +138,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
