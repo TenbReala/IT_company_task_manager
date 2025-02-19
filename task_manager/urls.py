5c9bbbe2cbcc69671940b668306fc224fa7cb557
Viktor Zhdanovych <cryingtm27@gmail.com>
2025-02-19 14:47:17 +0200

    CRUD for model=worker/position without templates

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,16 +1,36 @@
 from django.urls import path
 
-from task_manager.views import index, WorkerCreateView, ProjectsCreateView, ProjectsListView, ProjectsUpdateView, \
-    ProjectsDeleteView, ProjectsDetailView
+from task_manager.views import (
+    index,
+    WorkerCreateView,
+    ProjectsCreateView,
+    ProjectsListView,
+    ProjectsUpdateView,
+    ProjectsDeleteView,
+    ProjectsDetailView, PositionListView, PositionCreateView, PositionDetailView, PositionUpdateView,
+    PositionDeleteView, WorkerListView, WorkerUpdateView, WorkerDetailView, WorkerDeleteView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("worker/create/", WorkerCreateView.as_view(), name="worker_create"),
+    path("worker/", WorkerListView.as_view(), name="worker_list"),
+    path("worker/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker_update"),
+    path("worker/<int:pk>/detail/", WorkerDetailView.as_view(), name="worker_detail"),
+    path("worker/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker_delete"),
+
+
     path("projects/", ProjectsListView.as_view(), name="projects_list"),
     path("projects/create/", ProjectsCreateView.as_view(), name="project_create"),
     path("projects/<int:pk>/detail/", ProjectsDetailView.as_view(), name="project_detail"),
     path("projects/<int:pk>/update/", ProjectsUpdateView.as_view(), name="project_update"),
     path("projects/<int:pk>/delete/", ProjectsDeleteView.as_view(), name="project_delete"),
+
+    path("position/", PositionListView.as_view(), name="position_list"),
+    path("position/create/", PositionCreateView.as_view(), name="position_create"),
+    path("position/<int:pk>/detail/", PositionDetailView.as_view(), name="position_detail"),
+    path("position/<int:pk>/update/", PositionUpdateView.as_view(), name="position_update"),
+    path("position/<int:pk>/delete/", PositionDeleteView.as_view(), name="position_delete"),
     ]
 
 app_name = "task_manager"
--- task_manager/views.py
+++ task_manager/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 
 from task_manager.forms import WorkerCreationForm
-from task_manager.models import Worker, Task, Project, Team
+from task_manager.models import Worker, Task, Project, Team, Position
 
 
 @login_required
@@ -31,6 +31,25 @@ class WorkerCreateView(CreateView):
     form_class = WorkerCreationForm
 
 
+class WorkerListView(ListView):
+    model = Worker
+
+
+class WorkerUpdateView(UpdateView):
+    model = Worker
+    form_class = WorkerCreationForm
+
+
+class WorkerDetailView(DetailView):
+    model = Worker
+    queryset = Worker.objects.all().prefetch_related("assignees__task_type").select_related("position")
+
+
+class WorkerDeleteView(DeleteView):
+    model = Worker
+    success_url = reverse_lazy("")
+
+
 class ProjectsListView(ListView):
     model = Project
     template_name = "task_manager/projects_list.html"
@@ -45,6 +64,7 @@ class ProjectsCreateView(CreateView):
 
 class ProjectsDetailView(DetailView):
     model = Project
+    queryset = Project.objects.all().prefetch_related("tasks")
 
 
 class ProjectsUpdateView(UpdateView):
@@ -57,3 +77,25 @@ class ProjectsUpdateView(UpdateView):
 class ProjectsDeleteView(DeleteView):
     model = Project
     success_url = reverse_lazy("task_manager:projects_list")
+
+
+class PositionListView(ListView):
+    model = Position
+
+
+class PositionCreateView(CreateView):
+    model = Position
+    fields = "__all__"
+
+
+class PositionDetailView(DetailView):
+    model = Position
+
+
+class PositionUpdateView(UpdateView):
+    model = Position
+    fields = "__all__"
+
+
+class PositionDeleteView(DeleteView):
+    model = Position
